e7888a8d522f418a0974d3ba6cd624a46b66e988
Collin Bardini <cbardini@osisoft.com>
2021-07-22 15:03:15 -0700

    Update UnitTest1.cs

--- OCSPowerQueryTest/UnitTest1.cs
+++ OCSPowerQueryTest/UnitTest1.cs
@@ -30,7 +30,7 @@ namespace OCSPowerQueryTest
         /// <returns>true if successful</returns>
         private static bool testPowerQueryFunction(string powerQueryFile)
         {
-            bool success;
+            bool success = false;
 
             // Read in power query script to run
             var pqScript = "";
@@ -58,53 +58,61 @@ namespace OCSPowerQueryTest
                     var window = app.GetMainWindow(automation);
                     var desktop = window.Parent;
 
-                    // Close the start window
-                    var koStart = WaitForElement(() => window.FindFirstChild(cf => cf.ByAutomationId("KoStartDialog")));
-                    var getDataButton = WaitForElement(() => koStart.FindFirstDescendant(cf => cf.ByName("Get data"))?.AsButton());
-                    getDataButton?.Invoke();
+                    try 
+                    {
+                        // Close the start window
+                        var koStart = WaitForElement(() => window.FindFirstChild(cf => cf.ByAutomationId("KoStartDialog")));
+                        var getDataButton = WaitForElement(() => koStart.FindFirstDescendant(cf => cf.ByName("Get data"))?.AsButton());
+                        getDataButton?.Invoke();
 
-                    // Select Blank Query
-                    var dataSource = WaitForElement(() => window.FindFirstDescendant(cf => cf.ByAutomationId("DataSourceGalleryDialog")));
-                    var searchBar = WaitForElement(() => dataSource.FindFirstDescendant(cf => cf.ByName("Search")));
-                    searchBar.AsTextBox().Text = "Blank Query";
-                    var blankQueryListItem = WaitForElement(() => dataSource.FindFirstDescendant(cf => cf.ByName("Blank Query"))?.AsListBoxItem());
-                    blankQueryListItem?.WaitUntilClickable(TimeSpan.FromSeconds(30));
-                    blankQueryListItem?.Click();
-                    var connectButton = dataSource.FindFirstDescendant(cf => cf.ByName("Connect"))?.AsButton();
-                    connectButton?.Invoke();
+                        // Select Blank Query
+                        var dataSource = WaitForElement(() => window.FindFirstDescendant(cf => cf.ByAutomationId("DataSourceGalleryDialog")));
+                        var searchBar = WaitForElement(() => dataSource.FindFirstDescendant(cf => cf.ByName("Search")));
+                        searchBar.AsTextBox().Text = "Blank Query";
+                        var blankQueryListItem = WaitForElement(() => dataSource.FindFirstDescendant(cf => cf.ByName("Blank Query"))?.AsListBoxItem());
+                        blankQueryListItem?.WaitUntilClickable(TimeSpan.FromSeconds(30));
+                        blankQueryListItem?.Click();
+                        var connectButton = dataSource.FindFirstDescendant(cf => cf.ByName("Connect"))?.AsButton();
+                        connectButton?.Invoke();
 
-                    // Get the power query editor window
-                    var queryEditor = WaitForElement(() => desktop.FindFirstChild(cf => cf.ByAutomationId("QueriesEditorWindow")));
+                        // Get the power query editor window
+                        var queryEditor = WaitForElement(() => desktop.FindFirstChild(cf => cf.ByAutomationId("QueriesEditorWindow")));
 
-                    // Open the advanced editor
-                    var advancedEditorButton = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByName("Advanced Editor"))?.AsButton());
-                    advancedEditorButton?.Invoke();
+                        // Open the advanced editor
+                        var advancedEditorButton = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByName("Advanced Editor"))?.AsButton());
+                        advancedEditorButton?.Invoke();
 
-                    // Get the advanced editor window
-                    var advancedEditor = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByAutomationId("ViewFormulaDialog")));
+                        // Get the advanced editor window
+                        var advancedEditor = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByAutomationId("ViewFormulaDialog")));
 
-                    // Copy script into the advanced editor text field
-                    var advancedEditorEdit = WaitForElement(() => advancedEditor.FindFirstDescendant(cf => cf.ByControlType(ControlType.Edit))?.AsTextBox());
-                    advancedEditorEdit.Text = $"{pqScript}";
-                    var doneButton = WaitForElement(() => advancedEditor.FindFirstDescendant(cf => cf.ByName("Done"))?.AsButton());
-                    doneButton?.Invoke();
+                        // Copy script into the advanced editor text field
+                        var advancedEditorEdit = WaitForElement(() => advancedEditor.FindFirstDescendant(cf => cf.ByControlType(ControlType.Edit))?.AsTextBox());
+                        advancedEditorEdit.Text = $"{pqScript}";
+                        var doneButton = WaitForElement(() => advancedEditor.FindFirstDescendant(cf => cf.ByName("Done"))?.AsButton());
+                        doneButton?.Invoke();
 
-                    // Invoke the power query function
-                    var invokeButton = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByName("Invoke"))?.AsButton());
-                    invokeButton?.Invoke();
+                        // Invoke the power query function
+                        var invokeButton = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByName("Invoke"))?.AsButton());
+                        invokeButton?.Invoke();
 
-                    // Complete data privacy question
-                    var continueButton = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByName("Continue"))?.AsButton());
-                    continueButton?.Invoke();
-                    var privacyLevels = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByAutomationId("FirewallDialog")));
-                    var checkBox = WaitForElement(() => privacyLevels.FindFirstDescendant(cf => cf.ByControlType(ControlType.CheckBox))?.AsCheckBox());
-                    checkBox?.Click();
-                    var saveButton = privacyLevels.FindFirstDescendant(cf => cf.ByName("Save"))?.AsButton();
-                    saveButton?.Invoke();
+                        // Complete data privacy question
+                        var continueButton = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByName("Continue"))?.AsButton());
+                        continueButton?.Invoke();
+                        var privacyLevels = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByAutomationId("FirewallDialog")));
+                        var checkBox = WaitForElement(() => privacyLevels.FindFirstDescendant(cf => cf.ByControlType(ControlType.CheckBox))?.AsCheckBox());
+                        checkBox?.Click();
+                        var saveButton = privacyLevels.FindFirstDescendant(cf => cf.ByName("Save"))?.AsButton();
+                        saveButton?.Invoke();
 
-                    // Check if the query was successful
-                    var dataGrid = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByControlType(ControlType.DataGrid)));
-                    success = dataGrid != null;
+                        // Check if the query was successful
+                        var dataGrid = WaitForElement(() => queryEditor.FindFirstDescendant(cf => cf.ByControlType(ControlType.DataGrid)));
+                        success = dataGrid != null;
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("An error occurred while testing the provided power query script:");
+                        Console.WriteLine(e.Message);
+                    }
 
                     app.Close();
                 }
